05c4c825e3560a019c56c54c7e4f5a73d7b5eefb
jhon112 <jhonarmont@hotmail.com>
2019-09-27 00:24:04 +0000

    Add function to return the instruction according to the input read from file. Include error in case the instruction is not correct

---
+++ main_functions.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * get_instruction - returns the instruction according to the opcode found
+ * @opcode: token gotten from file
+ * @line_number: number of line were instructions was provided
+ * @line: line read from file. Use to free in error case
+ * @fl: file opened. Use to fclose in error case
+ *
+ * Return: pointer to struct instruction requested
+ */
+instruction_t get_instruction(char *opcode, int line_number, char *line,
+			      FILE *fl)
+{
+	instruction_t instructions[] = {{"push", NULL}, {"pall", NULL}};
+	int length = 0, i = 0;
+
+	length = (sizeof(instructions) / sizeof(instruction_t));
+	while (i < length)
+	{
+		if (_strcmp(instructions[i].opcode, opcode) == 0)
+			return (instructions[i]);
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	free(line);
+	fclose(fl);
+	exit(EXIT_FAILURE);
+}
