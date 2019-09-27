32e370b54845f26b0b46d31cbe3a80a6d927ef85
jhon112 <jhonarmont@hotmail.com>
2019-09-27 01:40:35 +0000

    File with functions to handle opcode. At the moment we have push to add to stack and pall to print all the stack

---
+++ op_functions.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * push - add data to stack
+ * @stack: address of pointer to first node of stack(last added)
+ * @line_number: line number where the instruction was provided
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	(void)line_number;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		print_error("malloc");
+
+	new->n = data;
+	new->prev = NULL;
+	new->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - prints all nodes in stack
+ * @stack: address of pointer to first node stack(last item added)
+ * @line_number: line number of instruction
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
